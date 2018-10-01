0b34cb183fc0ed52dea3476e8fc502c54856e756
Jeter Medina <jonatanjeterm@gmail.com>
2018-10-01 18:43:32 -0500

    Con vidas y todo... Aun no funciona al 100

--- Crab.java
+++ Crab.java
@@ -8,6 +8,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Crab extends Actor
 {
+    private Counter contadorVidas;
+    private Counter contadorPuntuaje;
+    
+    public Crab()
+    {
+        contadorVidas = new Counter("Vida: ");;
+        contadorVidas.setValue(3);
+        contadorPuntuaje = new Counter("Puntos: ");;
+        contadorPuntuaje.setValue(0);
+    }
+    
+    protected void addedToWorld(World world)
+    {
+        world.addObject(contadorVidas,500,20);
+        world.addObject(contadorPuntuaje,400,20);
+    }
+    
     /**
      * Act - do whatever the Crab wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -23,6 +40,15 @@ public class Crab extends Actor
         }
         if(isTouching(Worm.class)){
             removeTouching(Worm.class);
+            contadorPuntuaje.setValue(contadorPuntuaje.getValue() + 100);
+            //Greenfoot.playSound("eating.wav");
+        }
+        if(isTouching(Lobster.class)){
+            contadorVidas.setValue(contadorVidas.getValue() - 1);
+            setLocation(257,361);
+        }
+        if(contadorVidas.getValue() == 0){
+            Greenfoot.stop();
         }
     }    
 }
--- CrabWorld.java
+++ CrabWorld.java
@@ -19,5 +19,42 @@ public class CrabWorld extends greenfoot.World
         super(560, 560, 1);
         Tiempo tiempo = new Tiempo();
         this.addObject(tiempo,60,20);
+        prepare();
+    }
+    
+    private void prepare()
+    {
+        Lobster lobster = new Lobster();
+        addObject(lobster,266,160);
+        Worm worm = new Worm();
+        addObject(worm,476,108);
+        Worm worm2 = new Worm();
+        addObject(worm2,187,69);
+        Worm worm3 = new Worm();
+        addObject(worm3,488,275);
+        Worm worm4 = new Worm();
+        addObject(worm4,45,167);
+        Worm worm5 = new Worm();
+        addObject(worm5,502,511);
+        Worm worm6 = new Worm();
+        addObject(worm6,38,305);
+        Worm worm7 = new Worm();
+        addObject(worm7,346,38);
+        Worm worm8 = new Worm();
+        addObject(worm8,83,48);
+        Worm worm9 = new Worm();
+        addObject(worm9,36,503);
+        Worm worm10 = new Worm();
+        addObject(worm10,232,500);
+        Crab cangrejo = new Crab();
+        addObject(cangrejo,257,361);
+        Worm worm11 = new Worm();
+        addObject(worm11,385,232);
+        Worm worm12 = new Worm();
+        addObject(worm12,408,416);
+        Worm worm13 = new Worm();
+        addObject(worm13,133,391);
+        Worm worm14 = new Worm();
+        addObject(worm14,158,239);
     }
 }
