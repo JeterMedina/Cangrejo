f10f291e4d7f52fad7aa2773e9a9945f8ef02ff1
Jeter Medina <jonatanjeterm@gmail.com>
2018-10-01 05:58:20 -0500

    Se agrego el tiempo al juego

--- CrabWorld.java
+++ CrabWorld.java
@@ -16,6 +16,8 @@ public class CrabWorld extends greenfoot.World
     public CrabWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(560, 560, 1); 
+        super(560, 560, 1);
+        Tiempo tiempo = new Tiempo();
+        this.addObject(tiempo,60,20);
     }
 }
